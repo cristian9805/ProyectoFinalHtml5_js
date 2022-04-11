f9c1599003f283e7110f6ef1adff96a9b83d5b8a
cristian9805 <cristianarguello92@gmail.com>
2022-04-10 21:01:07 -0600

    Validacion del login Y registro de Usuario terminado

--- Inicio/login.js
+++ Inicio/login.js
@@ -1,21 +1,28 @@
 function ValidarDatosLogin(){
-    var LocalStorage = JSON.parse(localStorage.getItem("persona"));
+    const form = document.getElementById("FormularioLogin"); 
+    let objPersona = JSON.parse(localStorage.getItem("persona"));
     
     //cedula y contraseña de la pantalla del login
-    let cedula = document.getElementById("txtId").value;
-    let contrasenna = document.getElementById("txtContrasenna").value;
+    let cedulaLog = document.getElementById("txtCedula").value;
+    let contrasennaLog = document.getElementById("txtContrasenna").value;
 
-    //cedula y contraseña del local starage
-    let cedulaGuardada= LocalStorage.CedulaRegistro;
-    let contrasennaGuardada = LocalStorage.Contrasenna1;
-    let nombre = LocalStorage.Nombres;
+
+    objPersona.forEach(per => {
+        if(per.CedulaRegistroUsu === cedulaLog && per.Contrasenna1 === contrasennaLog){           
+                console.log("La cedula es del usuario: ", per.Nombres);
+                alert("Bienvenido "+ per.Nombres);
+                window.location.href= "/Cliente/Buscar_viaje.html";
+        }else{ 
+            form.reset();
+        }
+    });                   
     
-    if(cedula == cedulaGuardada && contrasenna == contrasennaGuardada){
+    /*if(cedula == cedulaGuardada && contrasenna == contrasennaGuardada){
         alert("Hola "+nombre+" bienvenido");
         FormularioLogin.reset();
         window.location.href= "/Cliente/Buscar_viaje.html";
     }else{
         alert("Usuario no valido, Registrese para empezar"); 
         FormularioLogin.reset();      
-    }
+    }*/
 }
--- Inicio/registroUsu.js
+++ Inicio/registroUsu.js
@@ -1,6 +1,60 @@
+function ObjPersona(){
+  const form = document.getElementById("formularioRegistroUsu"); 
+  /*form.addEventListener("submit",function(event){
+    event.preventDefault();
+    let transaFormData = new FormData(form);
+    let objPersonaDatos = ConvertirDatosformDataOBJ(transaFormData)
+    console.log(objPersonaDatos);
+  })*/
+  let transaFormData = new FormData(form);
+  let objPersonaDatos = ConvertirDatosformDataOBJ(transaFormData)
+  AlmacenarEnLocalStorage(objPersonaDatos)
+  form.reset();
+}
+function AlmacenarEnLocalStorage(objPersonaDatos){
+  // || esto significa ,que si esta vacio o null "||" entonces es un array vacio
+  let datos = JSON.parse(localStorage.getItem("persona")) || [];
+  datos.push(objPersonaDatos); 
+  let arrayJson = JSON.stringify(datos); 
+  localStorage.setItem("persona",arrayJson);
+  
+  window.alert("Usted se ah registrdo exitosamente");
+  window.location.href= "/Cliente/Buscar_viaje.html";
+  
+}
+
+function ConvertirDatosformDataOBJ(transaFormData){
+  let Apellido1 = transaFormData.get("txtApellido1");
+  let Apellido2 = transaFormData.get("txtApellido2");
+  let NumCel = transaFormData.get("txtNumTelefono");
+  let Nombres = transaFormData.get("txtNombres");
+  let FechaNaci = transaFormData.get("dateFechaNaci");
+  let CedulaRegistro = transaFormData.get("txtCedulaRegistroUsu");
+  let Contrasenna1 = transaFormData.get("txtContrasenna1");
+  let Contrasenna2 = transaFormData.get("txtContrasenna2");
+  return {
+    "Apellido1":Apellido1, 
+    "Apellido2": Apellido2, 
+    "NumTelefono":NumCel ,
+    "Nombres": Nombres,
+    "dateFechaNaci": FechaNaci,
+    "CedulaRegistroUsu": CedulaRegistro, 
+    "Contrasenna1": Contrasenna1,
+    "Contrasenna2": Contrasenna2
+  }
+}
 function GuardarDatosRegistroUsu(){   
-    //let cedulaEscrita = document.getElementById("txtCedulaRegistroUsu").value;             
-    let persona = {
+  ObjPersona();
+    /*let persona = [document.getElementById("txtApellido1").value,
+    document.getElementById("txtApellido2").value,
+    document.getElementById("txtNumTelefono").value,
+    document.getElementById("txtNombres").value,        
+    document.getElementById("dateFechaNaci").value,      
+    document.getElementById("txtCedulaRegistroUsu").value,
+    document.getElementById("txtContrasenna1").value,    
+    document.getElementById("txtContrasenna2").value
+    ];*/     
+    /*let persona = {
       Apellido1 : document.getElementById("txtApellido1").value,
       Apellido2 : document.getElementById("txtApellido2").value,
       NumCel : document.getElementById("txtNumTelefono").value,
@@ -9,9 +63,33 @@ function GuardarDatosRegistroUsu(){
       CedulaRegistro : document.getElementById("txtCedulaRegistroUsu").value,
       Contrasenna1 : document.getElementById("txtContrasenna1").value,
       Contrasenna2 : document.getElementById("txtContrasenna2").value
-    }       
-    localStorage.setItem("persona",JSON.stringify (persona));
+    }*/
+
+    let datos = JSON.parse(localStorage.getItem("persona"));
+    datos.push(persona); 
+    let arrayJson = JSON.stringify(datos); 
+    localStorage.setItem("persona",arrayJson);
+
     FormularioRegistroUsu.reset();
     window.alert("Usted se ah registrdo exitosamente");
     window.location.href= "login.html";
+ 
+    return persona;
+    
   };
+
+/*function ObtenerDaatosLocalStorage (){
+  let exist = true;
+  if(localStorage.getItem("persona")){
+    let per = JSON.parse(localStorage.getItem("persona"));   
+    console.log(per);
+    exist = true
+    console.log(exist);
+  }else{
+    console.log("No har datos registrados de personas")
+    exist = false;
+    console.log(exist);
+  }
+  return exist;
+}
+ObtenerDaatosLocalStorage();*/
